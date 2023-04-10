a8e6c845d68bdfaf4f71d3cf5a09e240a1c12869
ryeongeunkim <ryeongeun.gim@gmail.com>
2023-04-10 11:33:05 +0900

    이벤트 디테일 페이지 데이터 불러오기

--- src/pages/EventDetail.js
+++ src/pages/EventDetail.js
@@ -1,14 +1,27 @@
-import { useParams } from "react-router-dom";
+import { useLoaderData, json } from "react-router-dom";
+
+import EventItem from '../components/EventItem';
 
 function EventDetailPage() {
-  const params = useParams();
+  const data = useLoaderData();
 
   return (
-    <div>
-      <h1>EventDetailPage</h1>
-      <p>{params.eventId}</p>
-    </div>
+    <EventItem event={data.event} />
   );
 }
 
 export default EventDetailPage;
+
+export async function loader({request, params}) {
+  const id = params.eventId;
+
+  const response = await fetch(`/events/${id}`);
+
+  if (!response.ok) {
+    throw json({message: 'Could not fetch details for selected event.'}, {
+      status: 500
+    })
+  } else {
+    return response;
+  }
+}
