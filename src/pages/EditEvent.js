b7527d24fdf4c51ee9f96f63b6460b63ed0e758e
ryeongeunkim <ryeongeun.gim@gmail.com>
2023-04-10 13:57:20 +0900

    동적으로 요청하기, 수정 기능 추가

--- src/pages/EditEvent.js
+++ src/pages/EditEvent.js
@@ -4,7 +4,7 @@ import EventForm from "../components/EventForm";
 function EditEventPage() {
   const { event } = useRouteLoaderData("event-detail");
 
-  return <EventForm event={event} />;
+  return <EventForm method={'patch'} event={event} />;
 }
 
 export default EditEventPage;
